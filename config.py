a896f9cd508d525e425a575004a588b9a2f23a4d
maikeknauer <maike.knauer@study.thws.de>
2025-05-06 14:26:00 +0200

    add: Dateipfad zeitreihe_angeles für original und bereinigt

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 datapathzeitreiheberlin = "./daten/original-daten/zeitreihe_berlin.csv"
 datapathzeitreiheberlinbereinigt = "./daten/bereinigte-daten/zeitreihe_berlin_bereinigt.csv"
+
+datapathzeitreiheangeles = "./daten/original-daten/zeitreihe_angeles.csv"
+datapathzeitreiheangelesbereinigt = "./daten/bereinigte-daten/zeitreihe_angeles_bereinigt.csv"
