a33bdf12a21a90e6a390f861321667289d65da4b
marcwilhelm <marcwilli2015+github@gmail.com>
2025-05-08 19:00:52 +0200

    chore: csv files and config

--- config.py
+++ config.py
@@ -1,15 +1,19 @@
+import pandas as pd
+
 PATH_TS_BERLIN = "./daten/original-daten/zeitreihe_berlin.csv"
-PATH_TS_BERLIN_CLEAN = "./daten/bereinigte-daten/zeitreihe_berlin_bereinigt.csv"
+PATH_TS_BERLIN_CLEAN = "./daten/bereinigte-daten/bereinigt_zeitreihe_berlin.csv"
 
 PATH_TS_ANGELES = "./daten/original-daten/zeitreihe_angeles.csv"
-PATH_TS_ANGELES_CLEAN = "./daten/bereinigte-daten/zeitreihe_angeles_bereinigt.csv"
+PATH_TS_ANGELES_CLEAN = "./daten/bereinigte-daten/bereinigt_zeitreihe_angeles.csv"
 
-import pandas as pd
+PATH_TS_ABAKAN = "./daten/original-daten/zeitreihe_abakan.csv"
+PATH_TS_ABAKAN_CLEAN = "./daten/bereinigte-daten/bereinigt_zeitreihe_abakan.csv"
+
+df_berlin = pd.read_csv(PATH_TS_BERLIN_CLEAN)
+temp_diff_berlin = df_berlin['MonatlicheDurchschnittsTemperatur'].diff(1).dropna()
 
 df_angeles = pd.read_csv(PATH_TS_ANGELES_CLEAN)
 temp_diff_angeles = df_angeles['MonatlicheDurchschnittsTemperatur'].diff(1).dropna()
 
-
-
-PATH_TS_ABAKAN = "./daten/original-daten/zeitreihe_abakan.csv"
-PATH_TS_ABAKAN_CLEAN = "./daten/bereinigte-daten/bereinigt_zeitreihe_abakan.csv"
+df_abakan = pd.read_csv(PATH_TS_ABAKAN_CLEAN)
+temp_diff_abakan = df_abakan['MonatlicheDurchschnittsTemperatur'].diff(1).dropna()
